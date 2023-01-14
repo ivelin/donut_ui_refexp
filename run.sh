cab6d688a4794214c7f5d176147123b017458b0f
ivelin <ivelin.eth@gmail.com>
2023-01-14 20:21:44 +0000

    chore: checkpoint
    Signed-off-by: ivelin <ivelin.eth@gmail.com>

--- run.sh
+++ run.sh
@@ -23,7 +23,7 @@ source ./venv/bin/activate
 # https://cloud.google.com/tpu/docs/pytorch-xla-ug-tpu-vm
 # https://pytorch-lightning.readthedocs.io/en/stable/notebooks/lightning_examples/mnist-tpu-training.html
 export XRT_TPU_CONFIG="localservice;0;localhost:51011"
-export XLA_USE_BF16=0
+# export XLA_USE_BF16=0
 
 
 python3 -m ui_refexp.train --accelerator='tpu' --devices=1
