793214d6ba0a17c2db7a1f15850994d46db2acae
ivelin <ivelin.eth@gmail.com>
2023-01-14 21:02:05 +0000

    chore: checkpoint
    Signed-off-by: ivelin <ivelin.eth@gmail.com>

--- run.sh
+++ run.sh
@@ -4,8 +4,7 @@
 # Check TPU readiness
 # https://cloud.google.com/tpu/docs/users-guide-tpu-vm
 gcloud compute tpus tpu-vm list --zone=us-central1-f
-gcloud compute tpus tpu-vm describe  tpu-node-us-central-1f \
-  --zone=us-central1-f
+gcloud compute tpus tpu-vm describe  tpu-node-us-central-1f --zone=us-central1-f
 
 # https://cloud.google.com/tpu/docs/supported-tpu-configurations#pytorch
 
@@ -23,7 +22,7 @@ python3 -m torch_xla.core.xrt_run_server --port 51011 --restart
 # https://cloud.google.com/tpu/docs/pytorch-xla-ug-tpu-vm
 # https://pytorch-lightning.readthedocs.io/en/stable/notebooks/lightning_examples/mnist-tpu-training.html
 export XRT_TPU_CONFIG="localservice;0;localhost:51011"
-# export XLA_USE_BF16=0
+export XLA_USE_BF16=1
 
 
-python3 -m ui_refexp.train --accelerator='tpu' --devices=1
+python3 -m ui_refexp.train --accelerator='tpu' --devices=8
