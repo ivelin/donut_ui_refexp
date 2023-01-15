c473463868f10994c6041353348b205a56e26982
ivelin <ivelin.eth@gmail.com>
2023-01-15 16:30:29 +0000

    chore: checkpoint
    Signed-off-by: ivelin <ivelin.eth@gmail.com>

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@
 
 # Check TPU readiness
 # https://cloud.google.com/tpu/docs/users-guide-tpu-vm
-gcloud compute tpus tpu-vm list --zone=us-central1-f
-gcloud compute tpus tpu-vm describe  tpu2-us-central1-f --zone=us-central1-f
+# gcloud compute tpus tpu-vm list --zone=us-central1-f
+# gcloud compute tpus tpu-vm describe  tpu2-us-central1-f --zone=us-central1-f
 
 # https://cloud.google.com/tpu/docs/supported-tpu-configurations#pytorch
 
